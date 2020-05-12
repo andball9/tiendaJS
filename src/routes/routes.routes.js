849c50955c028012ab9753f6c72e2b85dbc59146
andball9 <andresballen9@gmail.com>
2020-05-12 15:38:41 -0500

    ajustes

--- src/routes/routes.routes.js
+++ src/routes/routes.routes.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('/usuarios',require('./Usuarios'));
-app.use('/productos',require('./Productos'))
+app.use('/productos',require('./Productos'));
+app.use('/Pagos',require('Pagos'));
+app.use('/Clientes',require('Clientes'));
 module.exports = app;
