8c9c9322877a9591d09140a0127a7d0fa84ee913
andball9 <andresballen9@gmail.com>
2020-05-12 15:49:54 -0500

    ./

--- src/routes/routes.routes.js
+++ src/routes/routes.routes.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use('/usuarios',require('./Usuarios'));
 app.use('/productos',require('./Productos'));
-app.use('/Pagos',require('Pagos'));
-app.use('/Clientes',require('Clientes'));
+app.use('/Pagos',require('./Pagos'));
+app.use('/Clientes',require('./Clientes'));
 module.exports = app;
